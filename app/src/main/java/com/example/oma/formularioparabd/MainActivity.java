6851d3e52d3158dde441ed787d6265514a86b8a0
derlyzflv <60946975+derlyzflv@users.noreply.github.com>
2020-09-09 17:39:56 -0500

    Fecha DataPicker

--- app/src/main/java/com/example/oma/formularioparabd/MainActivity.java
+++ app/src/main/java/com/example/oma/formularioparabd/MainActivity.java
@@ -2,20 +2,30 @@ package com.example.oma.formularioparabd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner comboproductos;
     EditText orden,cliente,fecha,cantidad,precio,total;
     Button agregar,mostrar;
+
+    private int dia, mes, ano;
+    private static DatePickerDialog.OnDateSetListener oyenteSelectorFecha;
+    private static final int TIPO_DIALOGO=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +44,21 @@ public class MainActivity extends AppCompatActivity {
         agregar=(Button) findViewById(R.id.agregar_boton);
         mostrar=findViewById(R.id.mostar_datos);
 
+        Calendar calendario=Calendar.getInstance();
+        ano=calendario.get(Calendar.YEAR);
+        mes=calendario.get(Calendar.MONTH);
+        dia=calendario.get(Calendar.DAY_OF_MONTH);
+        mostrarFecha();
+        oyenteSelectorFecha=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                ano=i;
+                mes=i1;
+                dia=i2;
+                mostrarFecha();
+            }
+        };
+
         final BD dblite=new BD(getApplicationContext());
 
         agregar.setOnClickListener(new View.OnClickListener() {
@@ -51,5 +76,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(mostrarVentas);
             }
         });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id){
+            case 0:
+                return new DatePickerDialog(this,oyenteSelectorFecha,ano,mes,dia);
+        }
+        return null;
+    }
+    public void mostrarCalendario(View control){
+        showDialog(TIPO_DIALOGO);
+    }
+    public void mostrarFecha() {
+        fecha.setText(ano + "/" + (mes + 1) + "/" + dia);
     }
 }
